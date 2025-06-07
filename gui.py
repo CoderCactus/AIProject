27cc4cecbde86d6404782508c4ca13f276a3b30a
Neda <20241896@novaims.unl.pt>
2025-06-07 23:03:53 +0100

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ from streamlit_drawable_canvas import st_canvas
 import cv2
 import main
 import pandas as pd
+from preprocessing import canvas_to_input_vector
 
 def classify(weights, input_vec):
     result = np.dot(input_vec, weights)
@@ -26,6 +27,11 @@ learning_rate = st.sidebar.slider(
 variable = st.sidebar.toggle('Variable Learning Rate')
 
 epochs = st.sidebar.slider("Training Epochs", 5000, 40000, 20000, step=2000)
+
+# Noise Optins
+st.sidebar.subheader("input options")
+add_noise = st.sidebar.checkbox("add noise", value = False)
+noise_lvl = st.sidebar.slider ("noise level", 0.0, 0.3, 0.05, 0.01)
 
 if st.sidebar.button("Load Saved Model"):
     try:
